1313853546325918fe454a5efcbd503e3137aa16
miftajuneidi2008 <miftajuneidi2008@gmail.com>
2023-05-06 14:23:36 +0300

    react Js Online Code Editor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const [htmls,setHtml] = useState('')
+  const [css,setCss] = useState('')
+  const [javascript,setJavascript] = useState('')
+  const handleClick = e=>
+  {
+    const outputs=document.getElementById("output");
+    outputs.contentDocument.body.innerHTML = htmls+'<style>'+css+'</style>';
+    outputs.contentWindow.eval(javascript)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header />
+    <div className="container">
+      <div className='left'>
+      <label>HTML</label>
+      <textarea name='html' onChange={(e)=>{setHtml(e.target.value)}} />
+
+      <label>CSS</label>
+      <textarea name='css'onChange={(e)=>{setCss(e.target.value)}} />
+
+      <label>Javascript</label>
+      <textarea name='js' onChange={(e)=>{setJavascript(e.target.value)}} />
+      </div>
+      <div className='right'>
+        <button onClick={handleClick}>Run</button>
+        <iframe id='output'></iframe>
+      </div>
+
     </div>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <h4>
+            React Js Online Code Editor
+        </h4>
+        </div>
+  )
+}
+
+export default Header
